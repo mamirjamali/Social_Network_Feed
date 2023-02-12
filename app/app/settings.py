a878d1b1c001f44edbcfcf4b8e721de2a9e0fbd0
mamirjamali <aj3848110@gmail.com>
2023-02-13 01:27:03 +0330

    Enable api documenting with drf_spactacular and swagger

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'drf_spectacular',
     'core',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -126,3 +129,7 @@ AUTH_USER_MODEL = 'core.User'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWOR = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
