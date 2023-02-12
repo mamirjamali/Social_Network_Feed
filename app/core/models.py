e1b3bcaf8f09fbf58581daf811df1c6a06bf3c64
mamirjamali <aj3848110@gmail.com>
2023-02-12 21:25:08 +0330

    Fix linting

--- app/core/models.py
+++ app/core/models.py
@@ -2,7 +2,11 @@
 Database Models
 """
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager
+)
 
 
 class ProfileManager(BaseUserManager):
