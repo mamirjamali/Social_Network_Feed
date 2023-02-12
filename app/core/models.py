58be5f833bd5f4a0e294a33b7ef2d3895c304017
mamirjamali <aj3848110@gmail.com>
2023-02-12 19:07:21 +0330

    Test and add superuser

--- app/core/models.py
+++ app/core/models.py
@@ -19,9 +19,9 @@ class ProfileManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, name, password):
+    def create_superuser(self, email, password, **extra_fields):
 
-        user = self.create_user(email, name, password)
+        user = self.create_user(email, password, **extra_fields)
 
         user.is_superuser = True
         user.is_staff = True
