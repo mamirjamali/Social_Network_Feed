1a5b13050d97d6ddeef964a0d6c61fa1bf9ed9f1
mamirjamali <aj3848110@gmail.com>
2023-02-12 21:22:36 +0330

    Test and add UserAdmin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,45 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from core import models
 
-# Register your models here.
+
+class AdminUser(BaseUserAdmin):
+    """Define user admin"""
+    ordering = ['id']
+    list_display = ['name', 'email']
+    fieldsets = [
+        [None, {'fields': ['name', 'email']}],
+        [_('Permissions'),
+         {'fields':
+            [
+                'is_active',
+                'is_superuser',
+                'is_staff'
+            ]
+          }
+         ],
+        [_('Important Dates'), {'fields': ['last_login']}]
+    ]
+    readonly_fields = ['last_login']
+    add_fieldsets = [
+        [None, {
+            'classes': ['wide'],
+            'fields': [
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser'
+            ]
+        }
+        ],
+    ]
+
+
+admin.site.register(models.User, AdminUser)
---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,45 @@
+"""
+Tests for Django admin.
+"""
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    """Test Admin panel."""
+
+    def setUp(self):
+        """Define setup for tests"""
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email="admin@example.com",
+            password="testpassword"
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email="test@example.com",
+            password="testpassword"
+        )
+
+    def test_user_list_in_admin_panel(self):
+        """Test if user is listed in the admin user page"""
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.user.email)
+
+    def test_user_detail_page_available(self):
+        """Test if the user page is available"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_add_user_page_available(self):
+        """Test if add_user page is available"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
